a2778cad8a0313cbad05ceb16ac41132d26c514f
idagn <111183857+idagn@users.noreply.github.com>
2022-08-13 18:39:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-document.write("TEST!");
+
 
 let del = prompt('Skal du med?');
 let message = 'Tak for at du svarede ' + del + '!';
       
 console.log(message);
 document.write(message);
-
+alert(message);
+document.write("TEST!");
